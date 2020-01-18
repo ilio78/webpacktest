409e084fe8eb7e77a6f03f99e1463fbbd9dd3fda
Giorgos Ilios <giorgos.ilios@gmail.com>
2020-01-18 21:10:31 +0100

    1st webpack test complete. Read doc for more info

---
+++ hello/app.js
@@ -0,0 +1,16 @@
+import {banana} from './banana.js';
+
+var msg = 'Hello Panda';
+console.log(msg);
+console.log(banana);
+//console.log(coke);
+
+export function showBananaAlerts()
+{
+    alert(banana);
+}
+
+export function showCokeAlerts()
+{
+    alert(coke);
+}
---
+++ hello/banana.js
@@ -0,0 +1,4 @@
+export var banana;
+
+var banana = "a yellow fruit that monkeys adore";
+var coke = "sugar water, not good for you";
